d26f7436b064599b9186baf34c712a968fd3fa8b
Ayesha Firdouse <ayeshamohammedycc@gmail.com>
2026-09-17 23:54:08 +0530

    practiced this maximal rectangle here

--- pra.py
+++ pra.py
@@ -1,8 +1,40 @@
-s=list("loewbi")
-t=list("pxezla")
-n=len(s)
-fre=[0]*n
-for i in range(n):
-    fre[i]=abs(ord(s[i])-ord(t[i]))
-for i in fre:
-    print(i,end=" ")
+
+def maximumArea(h):
+    m=len(h)
+    stack=[]
+    area=0
+    for i in range(m):
+        while stack and h[i]<h[stack[-1]]:
+            val=h[stack.pop()]
+            if stack:
+                width=i-stack[-1]-1
+            else:
+                width=i
+            area=max(area,val*width)
+        stack.append(i)
+    while stack:
+        val=h[stack.pop()]
+        if stack:
+            width=m-i-1
+        else:
+            width=m
+    area=max(area,val*width)
+    return area
+matrix=[["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
+n=len(matrix)
+m=len(matrix[0])
+h=list(int(ele) for ele in matrix[0])
+ref=list(int(ele) for ele in matrix[0])
+area=0
+for row in range(n):
+    for col in range(m):
+        if row>0 and matrix[row][col]=='1':
+            h[row][col]=ref[row][col]+1
+        else:
+            h[row][col]=0
+        ref[row][col]=h[row][col]
+        area=max(area,maximumArea(h))
+print(area)
+                
+       
+        
